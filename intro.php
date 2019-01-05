11ecbd5de7a2ab6ad302062c57ab0f1630701823
lupuselit <lupuselit@outlook.com>
2019-01-05 16:40:11 -0200

    add intro

---
+++ intro.php
@@ -0,0 +1,16 @@
+<?php
+
+  $name = "Emerson";
+
+  $arr = ['emerson', 'roberto', 'lucas'];
+  $arr2 = [['emerson', 'emerson'], 'another'];
+  $arr3 = [1,2,3,4,5];
+  function add($a, $b) {
+    return $a + $b;
+  }
+
+  $sum = add(3,4);
+
+  echo $sum;
+
+ ?>
